68c6419b12e6509f442eb558af36b8ac406379e8
Raymond <ray@firestitch.com>
2017-03-28 09:27:28 -0400

    initial commit

---
+++ src/app/scripts/directives/directive.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-overflow',[])
+    .directive('fsOverflow', function($location) {
+        return {
+            templateUrl: 'views/directives/overflow.html',
+            restrict: 'E',
+            transclude: true,
+            scope: {
+            	hasMore: '=?fsMore'
+            },
+            link: function($scope, element, attrs, ctrl, $transclude) {
+
+            	$scope.hasMore = $scope.hasMore===undefined ? true : $scope.hasMore;
+
+            	if($scope.hasMore) {
+					element.addClass('has-more');
+            	}
+
+            	$scope.moreClick = function() {
+            		$scope.more = !$scope.more;
+
+            		if($scope.more) {
+            			element.addClass('show');
+            		} else {
+            			element.removeClass('show');
+            		}
+            	}
+            }
+        };
+    });
+})();
